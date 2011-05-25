df2d719756ae280875f3dacbb959056c4756424a
Sascha Depold <sascha@dawanda.com>
2011-05-25 16:09:52 +0200

    tests

---
+++ test/helpers.js
@@ -0,0 +1,70 @@
+var Utils  = require(__dirname + "/../lib/utils")
+
+var Helpers = module.exports = {
+  exec:   require('child_process').exec,
+  fs:     require('fs'),
+  utils:  new Utils({
+    secret : "my-very-secret-secret",
+    magicHash : "magic",
+    port : 3000
+  }),
+  
+  serverRequests: 0,
+ 
+  clearTmpFolder: function() {
+    Helpers.exec('rm ' + process.cwd() + "/test/tmp/*")
+  },
+  clearTmpFolderBatch: {
+    'clear': {
+      topic: function() {
+        Helpers.clearTmpFolder()
+        this.callback()
+      },
+      'make it so': function(){}
+    }
+  },
+  
+  /*
+    path: the path you want to request
+    _options: a hash with options, possible options:
+                - toFile: will write the response into a file (e.g. for binary stuff)
+    _callback: a function to call after everything is done
+  */
+  requestServer: function(path, _options, _callback) {
+    var app      = require('../app')
+      , port     = !!app.fd ? app.address().port : (~~(Math.random() * 5000) + 2000)
+      , url      = "http://localhost:" + port + path
+      , options  = (typeof _options == 'function') ? {} : _options
+      , callback = (typeof _options == 'function') ? _options : _callback
+      , cmd      = "curl --silent '" + url + "'" + (options.toFile ? " > " + options.toFile : '')
+
+    Helpers.serverRequests++
+
+    if (!app.fd) {
+      app.listen(port)
+      console.log("Express server listening on port %d", app.address().port)
+    }
+    
+    console.log(cmd)
+
+    Helpers.exec(cmd, function(err, stdout, stderr) {
+      callback && callback(err, stdout, stderr)
+      if(--Helpers.serverRequests == 0) {
+        app.close()
+        app.__listening = false
+      }
+    })
+  }
+}
+
+Helpers.__defineGetter__('random', function() {
+  return ~~(Math.random() * 999999)
+})
+
+Helpers.__defineGetter__('testImageTargetPath', function() {
+  return process.cwd() + "/test/tmp/test" + Helpers.random +".gif"
+})
+
+Helpers.__defineGetter__('testImagePath', function() {
+  return process.cwd() + "/test/assets/test.gif"
+})
---
+++ test/image-magick.test.js
@@ -0,0 +1,57 @@
+var assert    = require('assert')
+  , vows      = require('vows')
+  , Helpers   = require("./helpers")
+  , srcImgUrl = encodeURIComponent("http://www.google.com/intl/en_ALL/images/logo.gif")
+  , im        = require('../lib/image-magick')
+
+vows.describe('image-magick').addBatch({
+  "convert with resize": {
+    topic: function() {
+      var cb = this.callback
+      im.convert('resize', Helpers.testImagePath, {size: '100x100'}, function(err, path){
+        Helpers.exec("identify " + path, cb)
+      })
+    },
+    "resizes images when size is passed": function(err, stdout, stderr) {
+      console.log(err, stdout)
+      assert.ok(typeof stdout != 'undefined')
+      assert.includes(stdout, '100x35')
+    }
+  },
+  "convert with fit": {
+    topic: function() {
+      var cb = this.callback
+      im.convert('fit', Helpers.testImagePath, {size: '100x100'}, function(err, path) {
+        Helpers.exec("identify " + path, cb)
+      })
+    },
+    "resizes images when size is passed": function(err, stdout, stderr) {
+      assert.ok(typeof stdout != 'undefined')
+      assert.includes(stdout, '100x100')
+    }
+  },
+  "convert with crop": {
+    topic: function() {
+      var cb = this.callback
+      im.convert('crop', Helpers.testImagePath, {crop: '10x15+20+25'}, function(err, path){
+        Helpers.exec("identify " + path, cb)
+      })
+    },
+    "resizes images when size is passed": function(err, stdout, stderr) {
+      assert.ok(typeof stdout != 'undefined')
+      assert.includes(stdout, '10x15')
+    }
+  },
+  "convert with crop and size": {
+    topic: function() {
+      var cb = this.callback
+      im.convert('crop', Helpers.testImagePath, {crop: '10x15+20+25', size: '100x20'}, function(err, path){
+        Helpers.exec("identify " + path, cb)
+      })
+    },
+    "resizes images when size is passed": function(err, stdout, stderr) {
+      assert.ok(typeof stdout != 'undefined')
+      assert.includes(stdout, '100x20')
+    }
+  }
+}).addBatch(Helpers.clearTmpFolderBatch).exportTo(module)
