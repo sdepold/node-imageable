2ff8ccc51b467baf520ddee86bd9a02dd43f7443
sdepold <sascha@depold.com>
2011-09-26 14:39:17 +0200

    improved identify parser

--- test/identify-parser.test.js
+++ test/identify-parser.test.js
@@ -5,13 +5,31 @@ var assert         = require('assert')
 vows.describe('identify-parser').addBatch({
   "#parse": {
     "should return json": function() {
-      assert.deepEqual(IdentifyParser.parse("foo: bar\n  a: b"), {a:'b'})
+      assert.deepEqual(IdentifyParser.parse("head: line\n  a: b"), {a:'b'})
     },
     "should parse keys with colons": function() {
-      assert.deepEqual(IdentifyParser.parse("foo: bar\n  foo:bar: bar"), {'foo:bar': 'bar'})
+      assert.deepEqual(IdentifyParser.parse("head: line\n  foo:bar: bar"), {'foo:bar': 'bar'})
     },
     "should parse values with colons": function() {
-      assert.deepEqual(IdentifyParser.parse("foo: bar\n  foo: hello:world"), {foo: 'hello:world'})
+      assert.deepEqual(IdentifyParser.parse("head: line\n  foo: hello:world"), {foo: 'hello:world'})
+    },
+    "should parse nested values": function() {
+      assert.deepEqual(IdentifyParser.parse("head: line\n  parent:\n    foo: bar"), {parent: {foo: 'bar'}})
+    },
+    "should parse super awesome constructs": function() {
+      var nested = "head: line\n  parent:\n    foo: bar\n    hello: world\n    child:\n      bla: blubb\n  parent2: normal"
+      var object = {
+        parent: {
+          foo: 'bar',
+          hello: 'world',
+          child: {
+            bla: 'blubb'
+          }
+        },
+        parent2: 'normal'
+      }
+
+      assert.deepEqual(IdentifyParser.parse(nested), object)
     }
   }
 }).exportTo(module)
