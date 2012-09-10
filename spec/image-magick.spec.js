48c6e05775775c9d758e44e77cbdc926c439e79a
sdepold <sascha@depold.com>
2012-09-10 09:42:31 +0200

    moved all image-magick tests to busterjs

--- spec/image-magick.spec.js
+++ spec/image-magick.spec.js
@@ -4,6 +4,7 @@ var buster      = require('buster')
   , exec        = require('child_process').exec
 
 buster.spec.expose()
+buster.testRunner.timeout = 10000
 
 describe('ImageMagick', function() {
   before(function() {
@@ -57,6 +58,137 @@ describe('ImageMagick', function() {
 
         this.moreDetailsPlease(/size-param/)
       })
+    })
+
+    describe('with url', function() {
+      it('resizes images when size is passed', function(done) {
+        this.resize({ size: '100x100', url: Helper.images.google }, function(err, stdout, stderr) {
+          expect(stdout).toBeDefined()
+          expect(stdout).toMatch('100x35')
+          done()
+        })
+      })
+    })
+
+    describe('with french url', function() {
+      it('resizes images when size is passed', function(done) {
+        this.resize({ size: '100x100', url: Helper.images.french }, function(err, stdout, stderr) {
+          expect(stdout).toBeDefined()
+          expect(stdout).toMatch('83x100')
+          done()
+        })
+      })
+    })
+
+    describe('with a big image', function() {
+      describe('without configured timeouts', function() {
+        it('resizes images when size is passed', function(done) {
+          this.resize({ size: '640x480', url: Helper.images.big }, function(err, stdout, stderr) {
+            expect(stdout).toBeDefined()
+            expect(stdout).toMatch('640x427')
+            done()
+          })
+        })
+      })
+
+      describe('with configured timeouts for convert', function() {
+        before(function() {
+          this.imageMagick = new ImageMagick({ timeouts: { convert: 100 } })
+        })
+
+        it('resizes images when size is passed', function(done) {
+          this.imageMagick.resize({ size: '640x480', url: Helper.images.big }, function(err, path) {
+            expect(err.killed).toBeTrue()
+            done()
+          })
+        })
+      })
+
+      describe('with configured timeouts for resize', function() {
+        before(function() {
+          this.imageMagick = new ImageMagick({ timeouts: { download: 100 } })
+        })
+
+        it('resizes images when size is passed', function(done) {
+          this.imageMagick.resize({ size: '640x480', url: Helper.images.big }, function(err, path) {
+            expect(err.killed).toBeTrue()
+            done()
+          })
+        })
+      })
+    })
+
+    describe('with size limit', function() {
+      beforeEach(function() {
+        this.imageMagick = new ImageMagick({ imageSizeLimit: 1000 })
+      })
+
+      it('works when size is below limit', function() {
+        expect(function() {
+          this.imageMagick.resize({ url: Helper.images.local, size: '100x200' }, function(){})
+        }.bind(this)).not.toThrow()
+      })
+
+      it('throws an error when size is above limit', function() {
+        expect(function() {
+          this.imageMagick.resize({ url: Helper.images.local, size: '1100x200' }, function(){})
+        }.bind(this)).toThrow()
+      })
+    })
+  })
+
+  describe('fit', function() {
+    it('resizes images to the passed size', function(done) {
+      this.fit({ size: '100x100', url: Helper.images.local }, function(err, stdout, stderr) {
+        expect(stdout).toBeDefined()
+        expect(stdout).toMatch('100x100')
+        done()
+      })
+    })
+  })
+
+  describe('crop', function() {
+    describe('without size', function() {
+      it('throws an error if no crop param is passed', function() {
+        expect(function() {
+          this.crop({ url: Helper.images.local }, function(){})
+        }.bind(this)).toThrow()
+        this.moreDetailsPlease(/crop-param/)
+      })
+
+      it('resizes images to the size defined in crop param', function(done) {
+        this.crop({ crop: '200x100+20+25', url: Helper.images.local }, function(err, stdout, stderr) {
+          expect(stdout).toBeDefined()
+          expect(stdout).toMatch('200x100')
+          done()
+        })
+      })
+    })
+
+    describe('with size', function() {
+      it('resizes images to passed size', function(done) {
+        this.crop({ crop: '200x400+20+25', size: '100x200', url: Helper.images.local }, function(err, stdout, stderr) {
+          expect(stdout).toBeDefined()
+          expect(stdout).toMatch('100x200')
+          done()
+        })
+      })
+    })
+  })
+
+  describe('missing image', function() {
+    it('is generated for invalid sources', function(done) {
+      this.crop({ crop: '200x400+20+25', size: '100x200', url: "http://foo.bar" }, function(err, stdout, stderr) {
+        expect(stdout).toBeDefined()
+        expect(stdout).toMatch('100x200')
+        done()
+      })
+    })
+  })
+
+  describe('_scaleCropInfo', function() {
+    it('scales the passed cropping info according to source size', function() {
+      expect(this.imageMagick._scaleCropInfo('200x300+10+10', ['375', ''], ['800', '600'])).toEqual('426x640+21+21')
     })
   })
 })
